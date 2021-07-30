c43cbf89c4fbef2b0147bdf5602f70b5760476cf
Iggy36 <42382841+tayloriggy@users.noreply.github.com>
2021-07-30 16:39:18 -0500

    Problem 1

---
+++ practiceProb.js
@@ -0,0 +1,12 @@
+/*
+Write a JavaScript function that accepts a string as a parameter 
+and find the longest word within the string.
+Steps:
+1. Define a function that accepts a string as a parameter
+2. Split the string that is passed in, turn it into an array of each individual word, and store it
+in a variable.
+3. Split each word string into an array of letters and store it in a variable.
+4. Define a variable called longestWord and set the inital value to 0
+5. Define a variable called wordLength and set the inital value to 0
+6.  
+*/
