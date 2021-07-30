e7ccd2e7cfa3daa7506d464957c9d899a6290d2d
Iggy36 <42382841+tayloriggy@users.noreply.github.com>
2021-07-30 16:40:55 -0500

    Problem 1

--- practiceProb.js
+++ practiceProb.js
@@ -8,5 +8,15 @@ in a variable.
 3. Split each word string into an array of letters and store it in a variable.
 4. Define a variable called longestWord and set the inital value to 0
 5. Define a variable called wordLength and set the inital value to 0
-6.  
+6. Start looping through the array of letters 
+7. Count how many letters are in each index(word)
+8. Return (print out) the longest word 
 */
+
+function findLongestWord(arrayOfWords) {
+    console.log(arrayOfWords);
+    arrayOfWords.split(" ");
+}
+
+arrayOfWords = ['Blue', 'Green', 'Black', 'Purple'];
+findLongestWord();
