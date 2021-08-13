87b4379f6f92285018552b845d29cafce1042af8
Iggy36 <42382841+tayloriggy@users.noreply.github.com>
2021-08-12 22:22:33 -0500

    probs update

--- prob4.js
+++ prob4.js
@@ -2,17 +2,25 @@
 A palindrome is a word, phrase, or sequence that reads the same backward as forward, e.g., madam or nurses run.
 Steps:
 1. Define a function called checkPalindrome and pass a string as a parameter.
-2. Define a variable called arrayOfStrings and change the string to lower case and remove all non-alphanumeric characters.
-3. Check to see if each character (arrayOfStrings[i]) reads the same going forward as they do going backwards.
-4. Reverse the string and if it is equal to the forward string passed then return true (it is a palindrome). If it is not equal, then return false
+2. Define a variable called len and set it equal to the length of the string passed in. 
+3. Change the string characters passed to lower case and remove all non-alphanumeric characters.
+4. Use the replace method to replace all non-alphanumeric characters with alphanumeric characters.
+5. Check to see if each character (arrayOfStrings[i]) reads the same going forward as they do going backwards.
+6. Reverse the string and if it is equal to the forward string passed then return true (it is a palindrome). If it is not equal, then return false
 (not a palindrome).
-4. Use a for loop
+7. Use a for loop
 
 
 
 */
 
 function checkPalindrome (str) {
-    var arrayOfStrings = str.toLowerCase().replace(/[^a-zA-Z0-9]+/g, "");
-    
+    var len = str.length;
+    for(var i = 0; i < len / 2; i++) {
+        if(str[i].toLowerCase().replace(/[^a-zA-Z0-9]+/g, "") !== str[len - 1 - i].toLowerCase().replace(/[^a-zA-Z0-9]+/g, "")) {
+            return "It is not a palindrome.";
+        }
+    }
+
+    return "It is a palindrome.";
 }
