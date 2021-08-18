11b231164d0356cf5627bd7c9da945c492741194
Iggy36 <42382841+tayloriggy@users.noreply.github.com>
2021-08-18 17:52:30 -0500

    prob update

--- prob5.js
+++ prob5.js
@@ -1,15 +1,13 @@
 /* Write a JavaScript program that iterates the integers from 1 to 100. But for multiples of three print "Fizz" instead of 
 the number and for the multiples of five print "Buzz". For numbers that are multiples of both three and five print "FizzBuzz".
 
-Everywhere you used a "for" loop previously, replace it with 
-either a #forEach, #map, or #filter method. Reference (study only forEach, map, or filter). 
 
 
 Steps:
 1. Write a for loop to iterate integers 1 to 100.
 2. Write an if condition that states if the integer is a multiple of 3 then print "Fizz", if a multiple of 5 then print "Buzz".
 3. If the integer is a multiple of both 3 and 5, then print "FizzBuzz".
-4. 
+4. That's it!
 
 
 
@@ -17,22 +15,21 @@ Steps:
 
 
 */
-var numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 
-    38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70,
-    71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99, 100];
-    
-numbers.forEach(isFizzOrBuzz);
+for (var i = 0; i < 100; i++) {
 
-function isFizzOrBuzz (num) {
-    if (num % 3 === 0) {
-        return("Fizz");
+    if (i % 3 === 0 && i % 5 === 0) {
+      console.log(i + " FizzBuzz");
     }
-    else if (num % 5 === 0) {
-        return("Buzz");
+  
+    else if (i % 3 === 0) {
+      console.log(i + " Fizz");
     }
-    else if (num % 3 === 0 && num % 5 === 0) {
-        return("FizzBuzz");
+   
+    else if (i % 5 === 0) {
+      console.log(i + " Buzz");
     }
-}
     
-    console.log(isFizzOrBuzz(numbers));
+  }
+    
+
+
